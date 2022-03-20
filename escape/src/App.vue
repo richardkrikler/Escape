796ede9b14828e404a4e49b3821923682ad8567f
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-03-20 19:32:45 +0100

    Added Vuex, Routing

--- escape/src/App.vue
+++ escape/src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <p>{{$store.state.test}}</p>
+
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
--- escape/src/main.js
+++ escape/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import routes from './routes'
+import store from './store'
+import {createStore} from 'vuex'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
-createApp(App).mount('#app')
+createApp(App).use(createStore(store)).use(createRouter({
+    history: createWebHashHistory(),
+    routes
+})).mount('#app')
