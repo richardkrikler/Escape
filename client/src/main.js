9fd49addf64ccccfc1756a60056a7391a3685fd7
Florexianer <8111@htl.rennweg.at>
2022-03-22 12:07:16 +0100

    Tailwind (not working)

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import routes from './routes'
 import store from './store'
 import {createStore} from 'vuex'
 import {createRouter, createWebHashHistory} from 'vue-router'
+import './assets/tailwind.css'
 
 createApp(App).use(createStore(store)).use(createRouter({
     history: createWebHashHistory(),
