6ef5366ba688807a3999544af6b09377430697bc
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-03-29 00:44:01 +0200

    edited basic landing-page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col justify-center min-h-screen place-items-center dark:bg-gray-800">
+    <div class="container">
+      <h1 class="text-7xl font-bold text-center border-2 dark:border-gray-700 rounded-xl p-3.5 m-2.5 text-gray-600 dark:text-gray-200">
+        Escape
+      </h1>
 
-  <p class="text-red-600">{{$store.state.test}}</p>
+      <div class="flex flex-row">
+        <timeline class="container"/>
+        <tasks class="container"/>
+      </div>
+    </div>
 
+    <p class="text-blue-600">{{ $store.state.test }}</p>
 
-  <router-view />
+    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <!--  <router-view />-->
+  </div>
 </template>
 
 <script>
@@ -13,12 +23,5 @@
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
