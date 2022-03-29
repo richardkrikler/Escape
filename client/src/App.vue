ba57373538b70ff87f3ba6ba8441e31e97ee94d1
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-03-29 10:24:39 +0200

    edited demo page

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,14 +4,9 @@
       <h1 class="text-7xl font-bold text-center border-2 dark:border-gray-700 rounded-xl p-3.5 m-2.5 text-gray-600 dark:text-gray-200">
         Escape
       </h1>
-
-      <div class="flex flex-row">
-        <timeline class="container"/>
-        <tasks class="container"/>
-      </div>
     </div>
 
-    <p class="text-blue-600">{{ $store.state.test }}</p>
+    <p class="text-blue-600 ">{{ $store.state.test }}</p>
 
     <!--    <img alt="Vue logo" src="./assets/logo.png">-->
     <!--  <router-view />-->
