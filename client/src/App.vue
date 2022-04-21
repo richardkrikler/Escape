5100f157f788a035d1b11b69876265d73d820dbb
Florexianer <8111@htl.rennweg.at>
2022-04-21 13:49:41 +0200

    text not selectable

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,10 +15,16 @@ export default {
 </script>
 
 <style lang="scss">
+
 * {
   font-family: adorn-condensed-sans, sans-serif;
   font-weight: 400;
   font-style: normal;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 
 </style>
