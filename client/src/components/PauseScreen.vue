7cc3e639b9112777844e4927131d10a3e5c96b26
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-04-25 08:24:12 +0200

    save game state from pause screen

--- client/src/components/PauseScreen.vue
+++ client/src/components/PauseScreen.vue
@@ -4,7 +4,8 @@
       <div class="flex justify-between">
         <h2 class="text-center text-8xl text-white mt-12 mb-2 text-title">PAUSE</h2>
         <div class="flex mt-12 mb-2">
-          <img src="../assets/media/images/schedule_white_24dp.svg" alt="clock-icon" class="clock-icon self-end mb-3 mx-2">
+          <img src="../assets/media/images/schedule_white_24dp.svg" alt="clock-icon"
+               class="clock-icon self-end mb-3 mx-2">
           <h2 class="text-center text-6xl text-white text-title self-end mx-2">{{ $store.getters.elapsedTime }}</h2>
         </div>
       </div>
@@ -48,7 +49,7 @@ export default {
     },
 
     saveGame() {
-
+      this.$store.commit('saveGame')
     },
 
     openSettings() {
