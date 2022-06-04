b9e6b3dc5a21b24d70527d5ef3fa1b97a97b0024
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-06-04 16:51:28 +0200

    updated imprint

--- src/components/ImprintScreen.vue
+++ src/components/ImprintScreen.vue
@@ -13,16 +13,20 @@
             UGB</b></p>
           <p class="mt-7 mb-1"><b>Webseitenbetreiber:</b> Richard Krikler</p>
           <p class="mt-7 mb-1"><b>Anschrift:</b> Rennweg, 89b, 1030 Wien</p>
-          <p class="mt-7 mb-1"><b>Kontaktdaten:</b> <br>Email: <a href="mailto:richard.krikler@htl.rennweg.at" class="text-glow">richard.krikler@htl.rennweg.at</a></p>
+          <p class="mt-7 mb-1"><b>Kontaktdaten:</b> <br>Email: <a href="mailto:richard.krikler@htl.rennweg.at"
+                                                                  class="text-glow">richard.krikler@htl.rennweg.at</a>
+          </p>
 
-          <p class="mt-7 mb-1"><b>Anwendbare Rechtsvorschrift:</b> <a href="https://www.ris.bka.gv.at" target="_blank" class="text-glow">www.ris.bka.gv.at</a> <br> <b>Berufsbezeichnung:</b>
+          <p class="mt-7 mb-1"><b>Anwendbare Rechtsvorschrift:</b> <a href="https://www.ris.bka.gv.at" target="_blank"
+                                                                      class="text-glow">www.ris.bka.gv.at</a> <br> <b>Berufsbezeichnung:</b>
             Schüler</p>
 
           <p class="mt-7 mb-1"><b>Online Streitbeilegung:</b> Verbraucher, welche in Österreich oder in einem sonstigen
             Vertragsstaat der
             ODR-VO niedergelassen sind, haben die Möglichkeit Probleme bezüglich dem entgeltlichen Kauf von Waren oder
             Dienstleistungen im Rahmen einer Online-Streitbeilegung (nach OS, AStG) zu lösen. Die Europäische Kommission
-            stellt eine Plattform hierfür bereit: <a href="https://ec.europa.eu/consumers/odr" target="_blank" class="text-glow">https://ec.europa.eu/consumers/odr</a></p>
+            stellt eine Plattform hierfür bereit: <a href="https://ec.europa.eu/consumers/odr" target="_blank"
+                                                     class="text-glow">https://ec.europa.eu/consumers/odr</a></p>
 
           <p class="mt-7 mb-1"><b>Urheberrecht:</b> Die Inhalte dieser Webseite unterliegen, soweit dies rechtlich
             möglich ist, diversen
@@ -39,20 +43,41 @@
             Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden
             derartiger Rechtsverletzungen werden wir den betroffenen Inhalt umgehend entfernen.</p>
 
-          <p class="mt-7 mb-1"><b>Quelle:</b> <a href="https://fairesrecht.at" target="_blank" class="text-glow">https://fairesrecht.at</a></p>
+          <p class="mt-7 mb-1"><b>Quelle:</b> <a href="https://fairesrecht.at" target="_blank" class="text-glow">https://fairesrecht.at</a>
+          </p>
 
 
           <div class="w-full bg-white mt-5 mb-16 h-0.5"></div>
 
           <h2 class="mt-7 mb-1 font-bold">Schriften</h2>
-          <p><a href="https://fonts.adobe.com/fonts/adorn-sans" target="_blank" class="text-glow">Adorn Condensed Sans</a></p>
-          <p>Lizenz: <a href="https://fonts.adobe.com/fonts/adorn-sans#licensing-section" target="_blank" class="text-glow">https://fonts.adobe.com/fonts/adorn-sans#licensing-section</a>
+          <p><a href="https://fonts.adobe.com/fonts/adorn-sans" target="_blank" class="text-glow">Adorn Condensed
+            Sans</a></p>
+          <p><a href="https://fonts.adobe.com/fonts/adorn-sans#licensing-section" target="_blank" class="text-glow">Adobe
+            Fonts Lizenz</a>
           </p>
 
           <h2 class="mt-7 mb-1 font-bold">Icons</h2>
           <p><a href="https://fonts.google.com/icons" target="_blank" class="text-glow">Google Material Icons</a></p>
-          <p>Lizenz: <a href="https://www.apache.org/licenses/LICENSE-2.0.html" target="_blank" class="text-glow">Apache License,
+          <p>Lizenz: <a href="https://www.apache.org/licenses/LICENSE-2.0.html" target="_blank" class="text-glow">Apache
+            License,
             Version 2.0</a></p>
+
+          <h2 class="mt-7 mb-1 font-bold">Soundeffekte</h2>
+          <p><a href="https://pixabay.com/de/sound-effects" target="_blank" class="text-glow">Pixabay Soundeffekte</a>
+          </p>
+          <p><a href="https://pixabay.com/de/service/license" target="_blank" class="text-glow">Pixabay Lizenz</a></p>
+          <p class="mt-3"><a href="https://www.zapsplat.com" target="_blank" class="text-glow">Zapsplat</a></p>
+          <p><a href="https://www.zapsplat.com/license-type/standard-license" target="_blank" class="text-glow">Zapsplat
+            Lizenz</a></p>
+
+          <h2 class="mt-7 mb-1 font-bold">Papier Grafik</h2>
+          <p>"Grey paper photo" von "rawpixel.com"</p>
+          <p><a href="https://www.freepik.com/free-photo/design-space-paper-textured-background_3220799.htm"
+                target="_blank" class="text-glow">Freepik Grafik</a></p>
+          <p><a href="https://www.freepikcompany.com/legal#nav-freepik" target="_blank" class="text-glow">Freepik
+            Lizenz</a></p>
+          <p><a href="https://support.freepik.com/s/article/Attribution-How-when-and-where" target="_blank"
+                class="text-glow">Freepik Attribution</a></p>
         </div>
       </div>
     </div>
